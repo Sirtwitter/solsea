8672c4f22cd60e8d7c5e08f9cb2831551d435e13
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2019-01-11 21:36:43 -0500

    Delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = article.find(params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to root_path
   end
 
   private
